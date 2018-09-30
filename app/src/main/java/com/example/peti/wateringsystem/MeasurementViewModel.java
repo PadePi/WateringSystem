596888d617452400c4f0da48a64380b73674702e
unknown <padezaninpeter1@gmail.com>
2018-09-30 19:53:34 +0200

    Added measurement view model

---
+++ app/src/main/java/com/example/peti/wateringsystem/MeasurementViewModel.java
@@ -0,0 +1,25 @@
+package com.example.peti.wateringsystem;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+
+import java.util.List;
+
+public class MeasurementViewModel extends AndroidViewModel {
+
+    private MeasurementRepository mRepository;
+    private LiveData<List<Measurement>> mAllMeasurements;
+
+    public MeasurementViewModel (Application application) {
+        super(application);
+        mRepository = new MeasurementRepository(application);
+        mAllMeasurements = mRepository.getmAllMeasurements();
+    }
+
+    LiveData<List<Measurement>> getmAllMeasurements() { return mAllMeasurements; }
+
+    public void insert(Measurement measurement) { mRepository.insert(measurement); }
+
+
+}
