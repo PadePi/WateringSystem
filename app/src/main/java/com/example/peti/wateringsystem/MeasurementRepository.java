a1713e18c852a2527f41be7f13f728a13a19333b
unknown <padezaninpeter1@gmail.com>
2018-09-30 19:45:55 +0200

    Added measurement repo

---
+++ app/src/main/java/com/example/peti/wateringsystem/MeasurementRepository.java
@@ -0,0 +1,44 @@
+package com.example.peti.wateringsystem;
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+
+import java.util.List;
+
+public class MeasurementRepository {
+
+    private MeasurementDAO mMeasurementDao;
+    private LiveData<List<Measurement>> mAllMeasurements;
+
+    MeasurementRepository(Application application) {
+        MeasurementRoomDatabase db = MeasurementRoomDatabase.getDatabase(application);
+        mMeasurementDao = db.measurementDao();
+        mAllMeasurements = mMeasurementDao.getAllMeasurement();
+    }
+
+    LiveData<List<Measurement>> getmAllMeasurements() {
+        return mAllMeasurements;
+    }
+
+
+    public void insert (Measurement measurement) {
+        new insertAsyncTask(mMeasurementDao).execute(measurement);
+    }
+
+    private static class insertAsyncTask extends AsyncTask<Measurement, Void, Void> {
+
+        private MeasurementDAO mAsyncTaskDao;
+
+        insertAsyncTask(MeasurementDAO dao) {
+            mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(final Measurement... params) {
+            mAsyncTaskDao.insert(params[0]);
+            return null;
+        }
+    }
+
+}
