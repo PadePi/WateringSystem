4dcfde2bbc4bc127988c2e23a3609ae020af1e65
zpadpet <peter.padezanin@sigmatechnology.se>
2018-11-04 18:57:38 +0100

    Created the requests on the arduino board that will be needed

--- SimpleWiFiServer/SimpleWiFiServer.ino
+++ SimpleWiFiServer/SimpleWiFiServer.ino
@@ -1,27 +1,7 @@
 /*
- WiFi Web Server LED Blink
 
- A simple web server that lets you blink an LED via the web.
  This sketch will print the IP address of your WiFi Shield (once connected)
- to the Serial monitor. From there, you can open that address in a web browser
- to turn on and off the LED on pin 5.
-
- If the IP address of your shield is yourAddress:
- http://yourAddress/H turns the LED on
- http://yourAddress/L turns it off
-
- This example is written for a network using WPA encryption. For
- WEP or WPA, change the Wifi.begin() call accordingly.
-
- Circuit:
- * WiFi shield attached
- * LED attached to pin 5
-
- created for arduino 25 Nov 2012
- by Tom Igoe
-
-ported for sparkfun esp32 
-31.01.2017 by Jan Hendrik Berlin
+ to the Serial monitor. From there, you can open that address in a web browser.
  
  */
 
@@ -30,12 +10,30 @@ ported for sparkfun esp32
 const char* ssid     = "UPC1179815";
 const char* password = "QOECXIIM";
 
+//Analog Input for moisure sensor
+#define ANALOG_PIN_MOISTURE 36
+int analog_value = 0;
+
+//Digital output for water pump
+#define DIGITAL_PIN_PUMP 2
+
+//Pins and variables for sonic sensor
+#define SONIC_TRIGGER 33
+#define SONIC_ECHO 32
+long duration;
+int distance;
+
+
+
 WiFiServer server(80);
 
 void setup()
 {
     Serial.begin(9600);
-    pinMode(2, OUTPUT);      // set the LED pin mode
+    pinMode(DIGITAL_PIN_PUMP, OUTPUT);      // set the water pump pin mode
+
+    pinMode(SONIC_TRIGGER, OUTPUT); // Sets the trigPin as an Output
+    pinMode(SONIC_ECHO, INPUT); // Sets the echoPin as an Input
 
     delay(10000);
 
@@ -81,16 +79,7 @@ void loop(){
           if (currentLine.length() == 0) {
             // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
             // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println();
 
-            // the content of the HTTP response follows the header:
-            client.print("Click <a href=\"/H\">here</a> to turn the LED on pin 5 on.<br>");
-            client.print("Click <a href=\"/L\">here</a> to turn the LED on pin 5 off.<br>");
-
-            // The HTTP response ends with another blank line:
-            client.println();
             // break out of the while loop:
             break;
           } else {    // if you got a newline, then clear currentLine:
@@ -101,13 +90,54 @@ void loop(){
         }
 
         // Check to see if the client request was "GET /H" or "GET /L":
-        if (currentLine.endsWith("GET /H")) {
-          digitalWrite(2, HIGH);               // GET /H turns the LED on
-          delay(10000);
-          digitalWrite(2, LOW); 
+        if (currentLine.endsWith("GET /measureMoisture")) {
+
+            analog_value = analogRead(ANALOG_PIN_MOISTURE);
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            client.print(analog_value);
+
+            // The HTTP response ends with another blank line:
+            client.println();
+            
+
         }
-        if (currentLine.endsWith("GET /L")) {
-          digitalWrite(2, LOW);                // GET /L turns the LED off
+        if (currentLine.endsWith("POST /startPump")) {
+          digitalWrite(DIGITAL_PIN_PUMP, HIGH);                // Start water pump
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-type:text/html");
+          client.println();
+        } if (currentLine.endsWith("POST /stopPump")) {
+          digitalWrite(DIGITAL_PIN_PUMP, LOW);                // Start water pump
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-type:text/html");
+          client.println();
+        }if (currentLine.endsWith("GET /waterLevel")) {
+          // Clears the trigPin
+          digitalWrite(SONIC_TRIGGER, LOW);
+          delayMicroseconds(2);
+          // Sets the trigPin on HIGH state for 10 micro seconds
+          digitalWrite(SONIC_TRIGGER, HIGH);
+          delayMicroseconds(10);
+          digitalWrite(SONIC_TRIGGER, LOW);
+          // Reads the echoPin, returns the sound wave travel time in microseconds
+          duration = pulseIn(SONIC_ECHO, HIGH);
+          // Calculating the distance
+          distance= duration*0.034/2;
+          // Prints the distance on the Serial Monitor
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-type:text/html");
+          client.println();
+
+          // the content of the HTTP response follows the header:
+          client.print(distance);
+
+          // The HTTP response ends with another blank line:
+          client.println();
+          
         }
       }
     }
