87495af85f5e8e284e10dc41a2e2ff9c84b009b2
Peter Padezanin <padezaninpeter1@gmail.com>
2018-12-01 19:37:13 +0100

    Added API calls for settings changes

--- app/src/main/java/com/example/peti/wateringsystem/SettingsFragment.java
+++ app/src/main/java/com/example/peti/wateringsystem/SettingsFragment.java
@@ -5,6 +5,17 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class SettingsFragment extends PreferenceFragment {
 
@@ -13,11 +24,16 @@ public class SettingsFragment extends PreferenceFragment {
     public static final String SCHEDULED_DAYS="scheduled_days";
     private SharedPreferences.OnSharedPreferenceChangeListener preferenceChangeListener;
 
+    private RequestQueue mRequestQueue;
+    private StringRequest stringRequest;
+    private String baseUrl="http://192.168.0.35/";
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings);
         final SharedPreferences sharedPref=this.getPreferenceManager().getSharedPreferences();
+        mRequestQueue= Volley.newRequestQueue(getContext());
 
         disabeOptions(sharedPref);
 
@@ -25,9 +41,105 @@ public class SettingsFragment extends PreferenceFragment {
         preferenceChangeListener=new SharedPreferences.OnSharedPreferenceChangeListener() {
             @Override
             public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-                if(key.equals(BASIC_BEHAVIOUR))
-                {
+                if(key.equals(BASIC_BEHAVIOUR)) {
                     disabeOptions(sharedPref);
+                    if (sharedPref.getString(BASIC_BEHAVIOUR, "").equals("Minimal water moisture")) {
+                        stringRequest = new StringRequest(Request.Method.GET, baseUrl + "minimalWater", new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                            }
+                        });
+                        mRequestQueue.add(stringRequest);
+                    }else
+                    {
+                        stringRequest = new StringRequest(Request.Method.GET, baseUrl + "scheduledDays", new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                            }
+                        });
+                        mRequestQueue.add(stringRequest);
+                    }
+                }
+                if(key.equals(MINIMAL_MOISTURE)) {
+                    if (sharedPref.getString(MINIMAL_MOISTURE, "").equals("Moisture under 40%")) {
+                        stringRequest = new StringRequest(Request.Method.GET, baseUrl + "under40", new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                            }
+                        });
+
+                        mRequestQueue.add(stringRequest);
+                    }
+                    if (sharedPref.getString(MINIMAL_MOISTURE, "").equals("Moisture under 50%")) {
+                        stringRequest = new StringRequest(Request.Method.GET, baseUrl + "under50", new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                            }
+                        });
+
+                        mRequestQueue.add(stringRequest);
+                    }
+                    if (sharedPref.getString(MINIMAL_MOISTURE, "").equals("Moisture under 60%")) {
+                        stringRequest = new StringRequest(Request.Method.GET, baseUrl + "under60", new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                            }
+                        });
+
+                        mRequestQueue.add(stringRequest);
+                    }
+                }
+                if(key.equals(SCHEDULED_DAYS))
+                {
+                    Set<String> days = sharedPref.getStringSet(SCHEDULED_DAYS, new HashSet<String>());
+                    String days_joined = String.join("", days);
+                    if(days_joined==""){
+                        days_joined="noDaySelected";
+                    }
+                    stringRequest = new StringRequest(Request.Method.GET, baseUrl + days_joined, new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+
+                        }
+                    });
+
+                    mRequestQueue.add(stringRequest);
+
                 }
             }
         };
